3761a2d4253b75ea031390d833181d68e89f3895
Daiius <yamashita.daiji@gmail.com>
2021-10-31 23:25:03 +0900

    add comment for reference

--- ColorEffectTest/Converters/ComparisonConverter.cs
+++ ColorEffectTest/Converters/ComparisonConverter.cs
@@ -7,6 +7,10 @@ using System.Windows.Data;
 
 namespace ColorEffectTest.Converters
 {
+    /// <summary>
+    /// Converts enum into IsChecked parameter, written with reference to
+    /// https://stackoverflow.com/questions/397556/how-to-bind-radiobuttons-to-an-enum
+    /// </summary>
     public class ComparisonConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
